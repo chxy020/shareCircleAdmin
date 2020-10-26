bea8cfb0af828275772d0ccc15234b05486e3c89
chenxy <chenxy@tingyun.com>
2020-10-26 23:23:52 +0800

    uid

--- views/operate/select_add_pop.js
+++ views/operate/select_add_pop.js
@@ -13,11 +13,20 @@ layui.config({
     data = '';
 
     var server = setter.baseUrl;
-
-    var uid = "5bea735b8c324eafbfd11b679eb758d01";
-
-    var server = setter.baseUrl;
     var uri = window.location.search;
+    var loginUrl = setter.loginUrl;
+
+    // var uid = "5bea735b8c324eafbfd11b679eb758d01";
+    var uid = "";
+    var userinfo = window.sessionStorage.getItem("__userinfo") || "";
+    if(userinfo){
+        userinfo = JSON.parse(userinfo);
+        uid = userinfo.uid;
+    }else{
+        location.href = loginUrl;
+        return;
+    }
+    
     
     // var types = ["","硬件故障","软件故障","硬件使用障碍","软件使用障碍","优化建议","投诉反馈","其它"];
     // var imgpaths = [];
